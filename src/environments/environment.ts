70435863e5b0d0807704841959de8540eac37b7c
Arda Yüksel <m.ardayuksell@gmail.com>
2024-07-29 12:29:05 +0300

    Eight

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     value: "development",
     // Diğer yapılandırma ayarları
-    api_key: "AIzaSyBp_dNNGl04ubZts_HapxrFEJWzQFAtWY8"
+    api_key: ""
 };
